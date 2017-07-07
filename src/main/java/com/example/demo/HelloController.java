4982b07b656e376cc62a02ac46e9574312b10e94
saikiran_sk20 <saikiran.seri@capgemini.com>
2017-07-07 10:32:18 -0400

    initial spring playground commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+/**
+ * Created by saseri on 7/6/2017.
+ */
+import java.lang.String;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloworld(){
+        return "Hello World!" ;
+
+    }
+
+
+}
